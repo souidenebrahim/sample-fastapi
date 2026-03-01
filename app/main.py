40105c81a6c8dd02450d7765865343931f1ce1e6
brahim souidene <souidenebrahim@gmail.com>
2026-03-01 06:58:09 +0100

    add html ui

--- app/main.py
+++ app/main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 # --------------------
 # Health endpoint
 # --------------------
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 async def health():
-    return {"message": "Hello World!"}
+    return """
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title>Home</title>
+        </head>
+        <body>
+            <h1>Hello World!</h1>
+            <p>Your API is running successfully 🚀</p>
+        </body>
+    </html>
+    """
